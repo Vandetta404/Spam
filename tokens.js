9e61f1d59926d8b02444690868116fc91fd2f7bc
Hurricane <69173408+xSimplez@users.noreply.github.com>
2020-10-14 14:55:06 +0200

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,12 +1,3 @@
-module.exports = ["NzQ2NTc4MTE5Njk1MjY5OTQ5.X0CXcQ.auna1qR8rGfid5ezd9PMUKPOVS8",
-                  "NzQ2NTgwMDg2MTkwNTA2MDA2.X0CZTg.8HDk16aQrGhjAxGwbUjjR_cFboE",
-                  "NzQ2NTgwODEyNTk3ODIxNTAw.X0CZ4w.vbex4op5vFuXYlL77kNn2iInQEA",
-                  "NzQ2NTgxMzgxNzIyNjY5MjM5.X0CaYA.6BTd-iBvbImvL5yeFcKJ4ojowtg",
-                  "NzQ2NTgxODM5NjM2MDcwNDYw.X0Ca_w.Daoul5Q5Lb5WUOVXwY7aCESCaUY",
-                  "NzQ2NTgyNTkyMTI3NjMxNDUx.X0CblA.BFvH0LmKOtvfuLyT3oGF-bUQTck",
-                  "NzQ2NTgzMTY4NjkzMzA1MzY1.X0CcEg.NZbF9fucW93CQKwyzaafEWjRzRw",
-                  "NzQ2NTgzODU2NDkyMTgzNjEz.X0Ccng.dDgzLLv1eN6_EaESJbTvYu5slD8",
-                  "NzQ2NTg1MDE0NjMxNjYxNjQ5.X0Cd5Q.7Q5gVUS_w7tbm_rxCWWifZ447YI",
-                  "NzQ2NTg1OTQwNjM3NTE1ODYz.X0CeuA.2OacWjClqZQaGOydI2cy-ZUdWYU"               
+module.exports = ["","","","",""              
                  
                  ]
